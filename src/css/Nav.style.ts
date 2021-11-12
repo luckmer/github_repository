0aca1c03e2083c66ae6d1801caa5957aacd24e1d
luckmer <woszczo04@gmail.com>
2021-11-12 12:04:35 +0100

    styles

---
+++ src/css/Nav.style.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  width: 100vw;
+  display: flex;
+  padding: 20px 50px 20px 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #007bff !important;
+`;
+export const Ul = styled.ul`
+  padding-right: 0.5rem;
+  padding-left: 0.5rem;
+  list-style: none;
+`;
+export const Header = styled.div`
+  display: inline-block;
+  padding-top: 0.3125 rem;
+  padding-bottom: 0.3125 rem;
+  margin-right: 1 rem;
+  line-height: inherit;
+  white-space: nowrap;
+`;
+export const LiHeader = styled.li`
+  color: #fff;
+  font-size: 1.25rem;
+  transition: all 0.5s;
+  &:hover {
+    color: rgb(255, 125, 147);
+  }
+`;
+export const ButtonLi = styled.li`
+  border-radius: 20px;
+  padding: 10px 30px 10px 30px;
+  background: none;
+  color: #fff;
+  border: 0 solid;
+  box-shadow: inset 0 0 20px rgba(255, 255, 255, 0);
+  outline: 1px solid;
+  outline-color: #f8f9fa;
+  outline-offset: 0px;
+  text-shadow: none;
+  transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
+  border: 1px solid #007bff;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid;
+    box-shadow: inset 0 0 20px rgba(255, 255, 255, 0.5),
+      0 0 20px rgba(255, 255, 255, 0.2);
+    outline-color: rgba(255, 255, 255, 0);
+    outline-offset: 15px;
+    text-shadow: 1px 1px 2px #427388;
+  }
+`;
