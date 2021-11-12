cbd77dcae3cf104d89ba2fbf3cbef11994823204
luckmer <woszczo04@gmail.com>
2021-11-12 12:02:24 +0100

    created navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,25 @@
-import styled from "styled-components";
+import { Nav, Header, Ul, LiHeader, ButtonLi } from "../css/Nav.style";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <Nav>
+      <Header>
+        <Ul>
+          <Link to="/">
+            <LiHeader>FINDER</LiHeader>
+          </Link>
+        </Ul>
+      </Header>
+      <Header>
+        <Ul>
+          <Link to="/help">
+            <ButtonLi>HELP</ButtonLi>
+          </Link>
+        </Ul>
+      </Header>
+    </Nav>
+  );
 };
 
 export default NavBar;
